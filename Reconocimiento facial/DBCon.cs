a11cd9eaae1422989cdfa1e66f88bebb293836db
EdwardJhosep <1454668@senati.pe>
2023-08-30 10:15:05 -0500

    modificar la funcion guardarimagen

--- Reconocimiento facial/DBCon.cs
+++ Reconocimiento facial/DBCon.cs
@@ -8,29 +8,44 @@ using System.IO;
 
 namespace Reconocimiento_facial
 {
+    // Definir la clase DBCon
     public class DBCon
     {
+        // Crear un campo para almacenar la conexión a la base de datos
         private OleDbConnection conn;
+
+        // Crear una lista para almacenar datos de usuarios
         public List<UserData> Users = new List<UserData>();
 
+        // Constructor de la clase, recibe la cadena de conexión a la base de datos
         public DBCon(string connectionString)
         {
+            // Inicializar la conexión utilizando la cadena de conexión proporcionada
             conn = new OleDbConnection(connectionString);
+            // Abrir la conexión
             conn.Open();
         }
-        public bool GuardarImagen(string Name, string Code, byte[] abImagen)
+    }
+    public bool GuardarImagen(string Name, string Code, byte[] abImagen)
+    {
+        try
         {
-            conn.Open();
-            OleDbCommand comm = new OleDbCommand("INSERT INTO UserFaces (Name,Code,Face) VALUES ('" + Name + "','" + Code + "',?)", conn);           
-            OleDbParameter parImagen = new OleDbParameter("@Face", OleDbType.VarBinary, abImagen.Length);
-            parImagen.Value = abImagen;
-            comm.Parameters.Add(parImagen);            
-            int iResultado = comm.ExecuteNonQuery();
-            conn.Close();
-            return Convert.ToBoolean(iResultado);
+            using (OleDbCommand comm = new OleDbCommand("INSERT INTO UserFaces (Name, Code, Face) VALUES (?, ?, ?)", conn))
+            {
+                comm.Parameters.AddWithValue("@Name", Name);
+                comm.Parameters.AddWithValue("@Code", Code);
+                comm.Parameters.AddWithValue("@Face", abImagen);
+                int iResultado = comm.ExecuteNonQuery();
+                return iResultado > 0;
+            }
         }
-
-        public DataTable ObtenerBytesImagen()
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            return false;
+        }
+    }
+    public DataTable ObtenerBytesImagen()
         {
             string sql = "SELECT IdImage,Name,Code,Face FROM UserFaces";
             OleDbDataAdapter adaptador = new OleDbDataAdapter(sql, conn);
