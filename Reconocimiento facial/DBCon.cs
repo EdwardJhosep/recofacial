14e364d05b923979d9f1e120a209d129ca4e7741
EdwardJhosep <1454668@senati.pe>
2023-08-30 10:04:56 -0500

    importamos nuevas bibliotecas

--- Reconocimiento facial/DBCon.cs
+++ Reconocimiento facial/DBCon.cs
@@ -4,9 +4,7 @@ using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Reconocimiento_facial
 {
